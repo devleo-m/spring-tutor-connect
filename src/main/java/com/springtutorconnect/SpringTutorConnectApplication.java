b73aa772aa5ee7a441504e886f459a3247613c0c
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-03-27 21:28:18 -0300

    first commit

---
+++ src/main/java/com/springtutorconnect/SpringTutorConnectApplication.java
@@ -0,0 +1,13 @@
+package com.springtutorconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTutorConnectApplication.class, args);
+    }
+
+}
