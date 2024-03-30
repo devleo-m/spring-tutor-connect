0248695c0488dc8359621d3823d9491717d34f18
dev-leomadeira <149954966+devleo-m@users.noreply.github.com>
2024-03-29 23:01:40 -0300

    Create ErrorHandler.java

---
+++ src/main/java/com/springtutorconnect/exception/ErrorHandler.java
@@ -0,0 +1,22 @@
+package com.springtutorconnect.exception;
+
+import com.springtutorconnect.exception.Error.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handler(Exception e){
+        return ResponseEntity.internalServerError().body(e.getMessage());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<?> handler(NotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+}
