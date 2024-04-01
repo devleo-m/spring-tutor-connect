f275664e220d86ab1387ec11eb69bf1a96160e2f
dev-leomadeira <149954966+devleo-m@users.noreply.github.com>
2024-04-01 16:06:30 -0300

    Update e correção de erros Repository/Agenda e Material

--- src/main/java/com/springtutorconnect/repository/AgendaRepository.java
+++ src/main/java/com/springtutorconnect/repository/AgendaRepository.java
@@ -2,8 +2,18 @@ package com.springtutorconnect.repository;
 
 import com.springtutorconnect.entity.AgendaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface AgendaRepository extends JpaRepository<AgendaEntity, Long>{
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface AgendaRepository extends JpaRepository<AgendaEntity, Long> {
+
+    List<AgendaEntity> findByAlunoIdOrderByData(Long alunoId);
+
+    List<AgendaEntity> findByAlunoIdAndDataGreaterThanOrderByData(Long alunoId, LocalDateTime data);
+
+    List<AgendaEntity> findByTutorIdOrderByData(Long tutorId);
+
+    List<AgendaEntity> findByTutorIdAndDataGreaterThanOrderByData(Long tutorId, LocalDateTime data);
+
 }
--- src/main/java/com/springtutorconnect/repository/MaterialRepository.java
+++ src/main/java/com/springtutorconnect/repository/MaterialRepository.java
@@ -1,9 +1,13 @@
 package com.springtutorconnect.repository;
 
+import com.springtutorconnect.entity.AgendaEntity;
 import com.springtutorconnect.entity.MaterialEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+
 public interface MaterialRepository extends JpaRepository<MaterialEntity, Long> {
+
+    List<MaterialEntity> findByAgenda(AgendaEntity agenda);
+
 }
